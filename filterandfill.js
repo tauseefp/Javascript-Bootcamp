4e1bdaba3b4257d9bcd355b05c140e332ba0a8b1
Patel tauseef <100660573+tauseefp@users.noreply.github.com>
2023-07-12 22:43:42 +0530

    Add files via upload

---
+++ filterandfill.js
@@ -0,0 +1,7 @@
+// let testarray=[2,3,4,5,6,7,8];
+// console.log(testarray.fill("h",2
+// ));
+const mynumber=[23,24,35,37,38,39];
+const result=mynumber.filter((Num)=>Num!=55);
+console.log(result);
+
---
+++ sliceandsplice.js
@@ -0,0 +1,9 @@
+// var users=["ted","tim","ton","sam","sor"]
+
+// console.log(user.slice(1,3));
+
+
+//Splice
+var users=["ted","Tim","sam","son"];
+users.splice(1,2,"string");
+console.log(users);
