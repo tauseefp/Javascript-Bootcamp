765a43a42240ff2152aaefe362a2d6e63c4e457d
Patel tauseef <100660573+tauseefp@users.noreply.github.com>
2023-07-24 12:03:03 +0530

    Add files via upload

---
+++ map.js
@@ -0,0 +1,7 @@
+let arr1=[1,233,4,5,7];
+let store=arr1.map((num)=>num*num);
+console.log(store);
+
+let names=["Tauseef","musaib","sana","ali","inaya"];
+let store1=names.map((name)=>name.toUpperCase());
+console.log(store1);
